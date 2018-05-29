57d22b1319b83055c7650778f330ff26bcf85de3
Henkenator <ljunggren.henrik@gmail.com>
2018-05-29 12:45:57 +0200

    the basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { StartComponent } from './components/start/start.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AssociatesComponent } from './components/associates/associates.component';
+import { CollaboratorsComponent } from './components/collaborators/collaborators.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {PracticeAreasComponent} from './components/practice-areas/practice-areas.component';
+
+
+export const appRoutes: Routes = [
+  {path: '', component: StartComponent},
+  {path: 'verksamhetsomraden', component: PracticeAreasComponent},
+  {path: 'medarbetare', component: AssociatesComponent},
+  {path: 'samarbeten', component: CollaboratorsComponent},
+  {path: 'kontakt', component: ContactComponent},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StartComponent,
+    ContactComponent,
+    AssociatesComponent,
+    CollaboratorsComponent,
+    PracticeAreasComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      //{ enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/components/practice-areas/practice-areas.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-practice-areas',
+  templateUrl: './practice-areas.component.html'
+})
+export class PracticeAreasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
